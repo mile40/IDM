3766e4e2d0dc1bcc67bcf57976b512f60492fe52
mile40 <emilemunch@gmail.com>
2021-01-28 15:11:14 +0100

    zer

--- src/LDPEngine.java
+++ src/LDPEngine.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Method;
 import java.util.HashMap;
 
 public class LDPEngine {
@@ -8,6 +9,25 @@ public class LDPEngine {
 		this.tags = tags;
 		this.fn = fileName;
 		this.bs = business;
+	}
+	
+	public Object dynamicInvoke(String methodName, Object target, Object params[]) throws Exception {
+		   Class cl = target.getClass();
+		   Class[] paramsClass = new Class[params.length];
+		   for (int i=0; i < params.length; i++)
+		      paramsClass[i] = params[i].getClass();
+		   Method met = cl.getMethod(methodName, paramsClass);
+		   Object result = met.invoke(target, params);
+		   return result;
+		}
+	
+	public void execute(String fileName, Calcul calc, HashMap<String, Object>  tags ) {
+		//TODO: TAMER
+	}
+	
+	public static void main() {
 		
 	}
+	
+	
 }
