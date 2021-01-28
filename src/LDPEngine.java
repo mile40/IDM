42b85705f7d51a15e8a6fbe56f93eef88e3dd84e
mile40 <emilemunch@gmail.com>
2021-01-28 15:11:05 +0100

    lol

--- src/LDPEngine.java
+++ src/LDPEngine.java
@@ -1,8 +1,13 @@
 import java.util.HashMap;
 
 public class LDPEngine {
-
+	String fn;
+	Object bs;
+	HashMap<String, Object> tags;
 	public void engine(String fileName, Object business, HashMap<String, Object> tags) {
+		this.tags = tags;
+		this.fn = fileName;
+		this.bs = business;
 		
 	}
 }
